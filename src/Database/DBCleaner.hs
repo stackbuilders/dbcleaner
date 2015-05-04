66734b22c2919a4a9940ded94cb301e80b4bc02c
Sebastián Estrella <sebas007estrella@gmail.com>
2015-05-04 10:07:26 -0500

    Merge all projects in one

--- src/Database/DBCleaner.hs
+++
@@ -1,7 +0,0 @@
-module Database.DBCleaner where
-
-import           Control.Exception        (bracket)
-import           Database.DBCleaner.Types (GenericConnection (..), Strategy)
-
-withConnection :: GenericConnection a => a -> Strategy -> (a -> IO b) -> IO b
-withConnection c s = bracket (before c s >> return c) (`after` s)
--- src/Database/DBCleaner/Types.hs
+++ src/Database/DBCleaner/Types.hs
@@ -1,7 +1,3 @@
 module Database.DBCleaner.Types where
 
 data Strategy = Transaction | Truncation
-
-class GenericConnection a where
-  before :: a -> Strategy -> IO ()
-  after :: a -> Strategy -> IO ()
